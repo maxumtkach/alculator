b3aa2d4c8bf5fe99902fc85f084545b5017bf7ae
maxumtkach <makstkach@bigmir.net>
2019-05-19 12:43:03 +0300

    //3.1.2 Действия калькулятора

---
+++ app/src/main/java/com/example/alculator/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.alculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String lastOperation = "="; // последняя операция
+    Double operand = null;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.text_view_edit);
+
+    }
+
+    public void onOperationClick(View view) {
+    }
+
+    // сохранение состояния
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if (operand != null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    public void onNumberClick(View view) {
+
+        Button button = (Button) view;
+        editText.append(button.getText());
+
+    }
+}
